295a3a06f346775416cd34a82a3004392da7a77a
penul4ik <maksimpenul@gmail.com>
2024-05-20 04:12:29 +0300

    hm

--- bash_learning/arg.sh
+++ bash_learning/arg.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-echo $1 $2 $3
+starttime=$(awk '{print $22}' /proc/153403/stat)
+starttime_in_seconds=$(echo "$starttime / 100" | bc)
+echo "starttime - $starttime_in_seconds"
+systemtime=$(cut -d' ' -f1 /proc/uptime)
+proctime=$(echo "$systemtime - $starttime_in_seconds" | bc)
+echo $proctime
+proctime_hours=$(echo "scale=2; $proctime / 3600" | bc)
+echo "Process uptime in hours: $proctime_hours"
