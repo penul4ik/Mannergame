c16e5f8c3edc08a2b7f5f2b206d942911e1bd216
m.konovalenko <m.konovalenko@qiwi.com>
2024-10-23 22:50:09 +0300

    Stepik

--- Golang/Stepik/4_6.go
+++ Golang/Stepik/4_6.go
@@ -5,14 +5,15 @@ import (
 )
 
 func main() {
-	n := 1
-	last := 1
+	var n int
+	fmt.Scan(&n)
+	last := n
 	var cnt int
 	for n != 0 {
 		if last < n {
 			cnt++
 		}
-		last += n
+		last = n
 		fmt.Scan(&n)
 	}
 	fmt.Println(cnt)
