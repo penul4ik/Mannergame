3364cb4e1a63ffc6ef562927646f977bb680380b
m.konovalenko <m.konovalenko@qiwi.com>
2025-01-28 05:08:47 +0300

    Something

--- Golang/Go_learning/code-ticket.go
+++ Golang/Go_learning/code-ticket.go
@@ -6,20 +6,37 @@ import (
 )
 
 func main() {
-	//startDay := 30
-	//startMouth := 10
-	//startYear := 2020
-	//distance := 62100000
-	fmt.Printf("Spaceline           Days  Trip type  Price\n")
-	fmt.Printf("==========================================\n")
+	distance := 62100000
+	minKmPerDays := 1382400
+	maxKmPerDays := 2592000
+	maxDays := distance / minKmPerDays // 44
+	minDays := distance / maxKmPerDays // 23
+	fmt.Printf("Spaceline           Days  Trip type     Price\n")
+	fmt.Printf("=============================================\n")
 	for i := 1; i <= 10; i++ {
-		CompanyName := "None"
+		companyName := "None"
+		tripType := "None"
+		days := 0
 		switch {
-		case rand.Intn(4)+1 == 1:
-			CompanyName = "VirginGalactic"
+		case rand.Intn(2)+1 == 1:
+			companyName = "VirginGalactic"
+		case rand.Intn(2)+1 == 2:
+			companyName = "Space Adventures"
 		default:
-			CompanyName = "SpaceX"
+			companyName = "SpaceX"
 		}
-		fmt.Printf("%-12v %11v %8v    $ %3v\n", CompanyName, "10", "One-way", "100")
+		switch {
+		case rand.Intn(2) == 1:
+			tripType = "Round-trip"
+		default:
+			tripType = "One-way"
+		}
+		for {
+			days = rand.Intn(maxDays) + 1
+			if days > minDays {
+				break
+			}
+		}
+		fmt.Printf("%-21v %2v  %-10v    $ %3v\n", companyName, days, tripType, days+20)
 	}
 }
